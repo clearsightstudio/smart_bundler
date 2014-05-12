971f4b74659efb3517ee4bc8887284ec94eeffef
Kevin VanGelder <kevin@clearsightstudio.com>
2014-05-12 16:30:19 -0700

    Version 0.0.5

--- lib/smart_bundler.rb
+++ lib/smart_bundler.rb
@@ -1,4 +1,4 @@
-require "commander/import"
+# require "commander/import"
 require "smart_bundler/version"
 require "smart_bundler/sh"
 require "smart_bundler/sb"
--- lib/smart_bundler/sb.rb
+++ lib/smart_bundler/sb.rb
@@ -8,11 +8,15 @@ module SmartBundler
     end
 
     def run
+      puts "Smart Bundler will recursively install your gems and output the successful bundler output. This may take a while."
+      count = 0
       while @finished != true
         result = call_bundle
         failed_gem = parse_output(result)
         install_failed_gem(failed_gem)
+        count += 1 unless @finished == true
       end
+      puts "#{count} gems"
     end
 
     def call_bundle
@@ -33,5 +37,5 @@ module SmartBundler
     end
 
   end
-  
+
 end
